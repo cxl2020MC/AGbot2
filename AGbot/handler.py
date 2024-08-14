f24f752fc822014c853988ca4a6671cebfb561c4
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-14 13:25:58 +0000

    test

--- AGbot/handler.py
+++ AGbot/handler.py
@@ -1,7 +1,8 @@
 from networkx import group_in_degree_centrality
 from .log import logger as log
 
-async def main(self, data, session):
+async def main(self, data: dict, session):
     if data.get("post_type") == "message":
         if data.get("message_type") == "group":
-            log.info(f"收到群 ({data.get('group_id')}) 内 {data.get('sender').get('card')}({data.get('sender').get('user_id')}) 的消息: {data.get('raw_message')} [{data.get('message_id')}]")
+            sender:dict = data.get("sender", {})
+            log.info(f"收到群 ({data.get('group_id')}) 内 {sender.get('card')}({sender.get('user_id')}) 的消息: {data.get('raw_message')} [{data.get('message_id')}]")
