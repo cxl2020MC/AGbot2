26dd8c901973701b97988871b601f9c51523dba2
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-14 14:10:28 +0000

    test

--- AGbot/handler.py
+++ AGbot/handler.py
@@ -4,7 +4,14 @@ async def main(self, data: dict, session):
     if data.get("post_type") == "message":
         if data.get("message_type") == "group":
             sender: dict = data.get("sender", {})
-            log.info(f"收到群 ({data.get('group_id')}) 内 {sender.get('card')}({sender.get('user_id')}) 的消息: {data.get('raw_message')} [{data.get('message_id')}]")
+            log.info(f"收到群 ({data.get('group_id')}) 内 {get_uername(sender)}({sender.get('user_id')}) 的消息: {data.get('raw_message')} [{data.get('message_id')}]")
         elif data.get("message_type") == "private":
             pass
         
+
+
+def get_uername(sender: dict) -> str:
+    if sender.get("card"):
+        return sender.get("card", "")
+    else:
+        return sender.get("nickname", "")
