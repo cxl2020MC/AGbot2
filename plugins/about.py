11659d4e436f82f846351667c7e31c9d18daed78
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-15 07:53:30 +0000

    test

--- plugins/about.py
+++ plugins/about.py
@@ -1,5 +1,6 @@
 from AGbot import plugin
 from AGbot.log import logger as log
+from AGbot import api
 
 bot = plugin.plugin("关于")
 
@@ -7,6 +8,6 @@ bot = plugin.plugin("关于")
 async def about(self, data, ws):
     log.info("收到关于命令")
 
-    await api.发送群消息(f"""关于AGbot2
+    await api.发送群消息(ws, data.get("group_id"), f"""关于AGbot2
 版本: 1.0
 作者: cxl2020mc""")
