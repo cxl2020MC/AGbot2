cf168746b273eac51e56e1895da5baf4c66d12b1
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-15 13:53:59 +0000

    test

--- AGbot/api.py
+++ AGbot/api.py
@@ -3,6 +3,7 @@ import time
 
 群信息缓存 = {}
 
+
 async def 获取群信息(ws, group_id):
     """
     获取群信息
@@ -10,18 +11,23 @@ async def 获取群信息(ws, group_id):
     await 刷新群信息缓存(ws, group_id)
     return 群信息缓存.get(group_id, {})
 
+
 async def 刷新群信息缓存(ws, group_id):
-    data = {"action": "get_group_info", "params": {"group_id": group_id}, "echo": {"type": "get_group_info", "group_id": group_id}}
+    data = {"action": "get_group_info", "params": {"group_id": group_id},
+            "echo": {"type": "get_group_info", "group_id": group_id}}
     await ws.send_json(data)
+
 
 async def 获取群名称(ws, group_id):
     return (await 获取群信息(ws, group_id)).get("group_name", "群名称加载中")
 
+
 async def 发送群消息(ws, group_id, message):
-    data = {"action": "send_group_msg", "params": {"group_id": group_id, "message": message}, "echo": {"type": "send_group_msg", "group_id": group_id}}
+    data = {"action": "send_group_msg", "params": {"group_id": group_id,
+                                                   "message": message}, "echo": {"type": "send_group_msg", "group_id": group_id}}
     await ws.send_json(data)
 
- 
+
 async def handler(ws, data):
     echo = data.get("echo")
     if echo.get("type") == "get_group_info":
--- AGbot/plugin.py
+++ AGbot/plugin.py
@@ -6,10 +6,12 @@ class _Bot:
     def __init__(self) -> None:
         self.插件列表 = []
         self.命令列表 = []
+
     def 加载插件(self, 插件):
         self.插件列表.append(插件)
         self.命令列表 += 插件.命令列表
         log.info(f"加载插件 {插件.名称} 成功")
+
     async def 匹配命令(self, bot_self, data, ws):
         """匹配命令"""
         消息 = data.get("message", "")
@@ -20,23 +22,23 @@ class _Bot:
                 if data["命令"] in 命令列表["命令列表"]:
                     log.debug(f"匹配到命令: {data['命令']}")
                     await 命令列表["函数"](bot_self, data, ws)
-        
 
 
 class Plugin:
     def __init__(self, 名称) -> None:
         self.名称 = 名称
         self.命令列表 = []
-    
+
     def 命令(self, 名称, 命令列表: list):
         def director(func):
             @functools.wraps(func)
             async def wrapper(*args, **kwargs):
                 return await func(*args, **kwargs)
-            命令数据 = {"命令列表": 命令列表, "命令名称":名称, "插件名称": self.名称, "函数": wrapper}
+            命令数据 = {"命令列表": 命令列表, "命令名称": 名称, "插件名称": self.名称, "函数": wrapper}
             self.命令列表.append(命令数据)
             log.debug(f"注册命令: {命令列表} 成功")
             return wrapper
         return director
+
 
 bot = _Bot()
