107145962c0d6ecc5b3feecb3e931be8a9bd1122
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-16 13:16:10 +0000

    test

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from AGbot import AGbot
 import asyncio
-from plugins import about
+from plugins import about, commend
 
 app = AGbot.App()
 app.插件.加载插件(about.bot)
+app.插件.加载插件(commend.bot)
 
 asyncio.run(app.run())
