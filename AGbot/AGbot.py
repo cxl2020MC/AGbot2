5ef61bb8e4854a320a75d7aaacd12649d25f1176
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-16 02:02:40 +0000

    test

--- AGbot/AGbot.py
+++ AGbot/AGbot.py
@@ -11,9 +11,10 @@ class App:
     def __init__(self, ws_url: str = "ws://localhost:3001/") -> None:
         self.ws_url = ws_url
         self.插件 = plugin.bot
-
+    
+    @utils.重试(重试次数=9999, 错误处理函数=None)
     async def run(self) -> None:
-        async def main(session: aiohttp.ClientSession):
+        async with aiohttp.ClientSession() as session:
             log.info(f"正在连接: {self.ws_url}")
             async with session.ws_connect(self.ws_url) as ws:
                 log.info("连接成功")
@@ -22,14 +23,3 @@ class App:
                     data = msg.json()
                     log.debug(f"收到json消息: {data}")
                     await handler.main(self, data, ws)
-
-        async with aiohttp.ClientSession() as session:
-            while True:
-                try:
-                    await main(session)
-                except:
-                    log.error(traceback.format_exc())
-                    log.error("连接断开, 将在3s后重连")
-                    await asyncio.sleep(3)
-
-
--- AGbot/utils.py
+++ AGbot/utils.py
@@ -1,4 +1,7 @@
 import aiohttp
+import asyncio
+import traceback
+from .log import logger as log
 
 
 def set_ws(websocket: aiohttp.ClientWebSocketResponse):
@@ -7,3 +10,18 @@ def set_ws(websocket: aiohttp.ClientWebSocketResponse):
 
 def get_ws() -> aiohttp.ClientWebSocketResponse:
     return ws
+
+def 重试(重试次数: int, 重试间隔: int = 1, 异常类型 = Exception, 错误处理函数 = None):
+    def directer(func):
+        async def wrapper(*args, **kwargs):
+            for i in range(重试次数):
+                try:
+                    return await func(*args, **kwargs)
+                except 异常类型 as e:
+                    log.error(f"发生错误, : {traceback.format_exc()}")
+                    log.info(f"将在 {重试间隔} 秒后重试第 {i+1} 次")
+                    if 错误处理函数:
+                        错误处理函数(e)
+                    await asyncio.sleep(重试间隔)
+        return wrapper
+    return directer
