4b33ff1ce8dff219e1d77b294f95b151da48803c
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-17 06:44:08 +0000

    test

--- plugins/commend.py
+++ plugins/commend.py
@@ -8,3 +8,7 @@ bot = plugin.Plugin("命令")
 async def commend(消息, data, ws):
     log.info("收到命令")
     await api.发送群消息(ws, data.get("group_id"), f"""识别命令为: {bot.解析命令(消息)}""")
+
+@bot.命令("抛出错误", ["/error"])
+async def error(消息, data, ws):
+    raise Exception("这是个主动抛出的错误")
