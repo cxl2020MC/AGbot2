f36d91a2597d7cf52573f8b2541e5cd36d07ffbd
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-23 07:22:59 +0000

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from AGbot import cilent, config, plugin
 import asyncio
-from plugins import about, commend
+from plugins import about, commend, mhy
 
 config.管理员QQ号 = [1176503930, 2130812665]
 config.机器人QQ号 = 1176503930
@@ -9,5 +9,7 @@ config.群聊白名单 = [860769764, 463566277, 959593907]
 
 plugin.bot.加载插件(about.bot)
 plugin.bot.加载插件(commend.bot)
+plugin.bot.加载插件(mhy.bot)
+
 
 asyncio.run(cilent.run())
