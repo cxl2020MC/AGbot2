54cdfed6abcd1b1adb5bd42157a83e9cd6261ead
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-23 07:30:30 +0000

    test

--- plugins/mhy.py
+++ plugins/mhy.py
@@ -12,6 +12,7 @@ async def 获取资讯(消息, data, ws):
     命令 = bot.解析命令(消息)
     游戏 = 命令["参数列表"][0]
     资讯 = await mhy_core.获取资讯("国服", 游戏)
+    log.debug(资讯)
 
     模板 = jinja2.Template("""轮播图:
 {% for i in data['banners'] %}
@@ -28,4 +29,3 @@ async def 获取资讯(消息, data, ws):
     消息 = 模板.render(data=资讯)
     await api.发送群消息(ws, data.get("group_id"), 消息)
 
-    await api.发送群消息(ws, data.get("group_id"), f"""识别命令为: {bot.解析命令(消息)}""")
