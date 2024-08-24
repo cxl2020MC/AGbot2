db8d80a0822d2580c57267c628693581a307c753
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-08-24 02:21:44 +0000

    test

--- plugins/mhy.py
+++ plugins/mhy.py
@@ -12,8 +12,8 @@ async def 获取资讯(消息, data, ws):
     命令 = bot.解析命令(消息)
     游戏 = 命令["参数列表"][0]
     game_id = await mhy_core.get_game_id(游戏)
-    data = await mhy_core.获取资讯("国服", game_id)
-    log.debug(资讯)
+    ret_data = await mhy_core.获取资讯("国服", game_id)
+    log.debug(ret_data)
 
     模板 = jinja2.Template("""轮播图:
 {% for i in data['banners'] %}[CQ:image,file={{i['image']['url']}}]
@@ -24,7 +24,7 @@ async def 获取资讯(消息, data, ws):
 {{i['link']}}
 {% endfor %}
 """)
-    消息 = 模板.render(data=data["data"].get("content"))
+    消息 = 模板.render(data=ret_data["data"].get("content"))
     await api.发送群消息(ws, data.get("group_id"), 消息)
 
 @bot.命令("获取启动器背景图", ["/获取启动器背景图"])
@@ -33,10 +33,10 @@ async def 获取启动器背景图(消息, data, ws):
     命令 = bot.解析命令(消息)
     游戏 = 命令["参数列表"][0]
     game_id = await mhy_core.get_game_id(游戏)
-    data = await mhy_core.获取游戏基本信息("国服", game_id)
-    log.debug(data)
+    ret_data = await mhy_core.获取游戏基本信息("国服", game_id)
+    log.debug(ret_data)
 
-    消息 = f"背景图:\n[CQ:image,file={data[0]['background']['url']}]"
+    消息 = f"背景图:\n[CQ:image,file={ret_data['game_info_list'][0]['background']['url']}]"
     await api.发送群消息(ws, data.get("group_id"), 消息)
 
 @bot.命令("获取游戏最新版本", ["/获取游戏最新版本"])
@@ -45,10 +45,10 @@ async def 获取游戏最新版本(消息, data, ws):
     命令 = bot.解析命令(消息)
     游戏 = 命令["参数列表"][0]
     game_id = await mhy_core.get_game_id(游戏)
-    data = await mhy_core.获取游戏安装包信息("国服", game_id)
-    log.debug(data)
+    ret_data = await mhy_core.获取游戏安装包信息("国服", game_id)
+    log.debug(ret_data)
 
     模板 = jinja2.Template("""最新版本: {{data['main']['major']['version']}}
 预下载: {% if data['pre_download']['major'] %}{{data['pre_download']['major']['version']}}{% else %}预下载未开启{% endif %}""")
-    消息 = 模板.render(data=data["data"]["game_packages"][0])
+    消息 = 模板.render(data=ret_data["data"]["game_packages"][0])
     await api.发送群消息(ws, data.get("group_id"), 消息)
