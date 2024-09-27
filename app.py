d6b0a7a8cf96380c9faa8f97c7a556da0f10bfbf
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-09-27 19:00:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from plugins import about, commend, mhy
 
 config.管理员QQ号 = [1176503930, 2130812665]
 config.机器人QQ号 = 1176503930
-config.群聊白名单 = [860769764, 463566277, 959593907]
+config.群聊白名单 = [860769764, 463566277, 959593907, 925603600]
 
 
 plugin.bot.加载插件(about.bot)
