cb566233b76dfa4ea1fc0a1288d861e06055bbbc
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2024-12-15 07:11:40 +0000

    test

--- AGbot/plugin.py
+++ AGbot/plugin.py
@@ -72,3 +72,51 @@ class Plugin:
             else:
                 command_data["参数列表"].append(参数)
         return command_data
+
+
+class CommandLineParser:
+    def __init__(self):
+        self.positional_args = []
+        self.options = {}
+
+    def add_argument(self, *args, **kwargs):
+        for arg in args:
+            self.options[arg] = kwargs.get('default', None)
+
+    def parse_args(self, command_str):
+        tokens = shlex.split(command_str)
+
+        for i, token in enumerate(tokens):
+            if token.startswith('-'):
+                if token in self.options:
+                    if i + 1 < len(tokens) and not tokens[i + 1].startswith('-'):
+                        self.options[token] = tokens[i + 1]
+                        i += 1
+                    elif self.options[token] is None:
+                        self.options[token] = True
+                    else:
+                        raise ValueError(f"Option {token} requires an argument")
+                else:
+                    raise ValueError(f"Unknown option: {token}")
+            else:
+                self.positional_args.append(token)
+
+        return self
+
+    def __str__(self):
+        return f"Positional arguments: {self.positional_args}\nOptions: {self.options}"
+
+# 创建解析器实例
+parser = CommandLineParser()
+
+# 添加命令行参数
+parser.add_argument('command', help='The main command')
+parser.add_argument('-f', '--file', help='File to process', default=None)
+parser.add_argument('-v', '--verbose', help='Enable verbose mode', action='store_true')
+
+# 解析命令行字符串
+command_str = 'main_command -f file.txt -v'
+parsed_args = parser.parse_args(command_str)
+
+# 打印解析结果
+print(parsed_args)
--- plugins/about.py
+++ plugins/about.py
@@ -9,6 +9,6 @@ async def about(消息, data):
     log.info("收到关于命令")
 
     await api.send_message(data, f"""关于AGbot2
-版本: 1.0-dev
+版本: 1.1.0-dev
 作者: cxl2020mc
 开源地址: https://github.com/cxl2020MC/AGbot2""")
