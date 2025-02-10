ba1a066882c455263e0ddcc2f10daa45c198b068
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-10 06:48:16 +0000

    test

--- plugins/help.py
+++ plugins/help.py
@@ -9,7 +9,7 @@ bot = plugin.Plugin("帮助")
 async def 帮助(消息, data):
     plugin_list = plugin.Plugin.plugin_list
     log.debug(f"plugin_list: {plugin_list}")
-    模板 = jinja2.Template("""帮助：{% for plugin_list in plugins %}{% for 命令 in plugin.command_list %}
+    模板 = jinja2.Template("""帮助：{% for pluginList in plugins %}{% for 命令 in plugin.command_list %}
     {{ 命令["命令名称"] }}: {{ 命令["command_list"] }}{% endfor %}{% endfor %}""")
-    消息内容 = 模板.render(plugin_list=plugin_list)
+    消息内容 = 模板.render(pluginList=plugin_list)
     await api.send_message(data, 消息内容)
