0e6b896461d6df827f258dd08026c8c41ee0b641
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-11 10:54:09 +0000

    test

--- plugins/mc.py
+++ plugins/mc.py
@@ -5,11 +5,23 @@ import mcstatus
 
 bot = plugin.Plugin("MC服务器")
 
+server_map = {
+    993426084: "ifeng.asia:1",
+    860769764: "home.cxl2020mc.top"
+}
+
 @bot.command("MC", ["mc", "查服"])
 async def about(消息, data):
     log.info("收到mc命令")
+
+    群号 = data.get("group_id")
+    if 群号 not in server_map:
+        await api.send_message(data, "该群没有配置mc服务器地址")
+        return
     
-    服务器 = await mcstatus.JavaServer.async_lookup("ifeng.asia:1")
+    服务器地址 = server_map[群号]
+    
+    服务器 = await mcstatus.JavaServer.async_lookup(服务器地址)
     延迟 = await 服务器.async_ping()
     状态 = await 服务器.async_status()
     log.debug(状态)
@@ -21,7 +33,7 @@ async def about(消息, data):
 
     await api.send_message(data, f"""服务器状态:
 版本: {状态.version.name}
-描述: {状态.motd}
+描述: {状态.motd.raw}
 延迟: {延迟:.2f}ms
 在线人数: {状态.players.online}/{状态.players.max}
 当前在线: {", ".join(当前在线)}""")
