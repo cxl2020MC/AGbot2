079f3ff0c38d88702ae415d82ad09b857697b7b1
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-11 10:00:42 +0000

    更新MC插件功能

--- plugins/mc.py
+++ plugins/mc.py
@@ -13,11 +13,14 @@ async def about(消息, data):
     延迟 = await 服务器.async_ping()
     状态 = await 服务器.async_status()
     # 查询 = await 服务器.async_query()
-
+    if 状态.players.sample:
+        当前在线 = [玩家.name for 玩家 in 状态.players.sample]
+    else:
+        当前在线 = "无"
 
     await api.send_message(data, f"""服务器状态:
+版本: {状态.version.name}
+描述: {状态.motd}
 延迟: {延迟}ms
 在线人数: {状态.players.online}/{状态.players.max}
-{状态}
-
-""")
+当前在线: {当前在线}""")
