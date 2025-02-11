abc13cfdb5c9f1d283612ec88e0c07a90431324c
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-11 09:35:18 +0000

    test

---
+++ plugins/mc.py
@@ -0,0 +1,23 @@
+from AGbot import plugin
+from AGbot.log import logger as log
+from AGbot import api
+import mcstatus
+
+bot = plugin.Plugin("MC服务器")
+
+@bot.command("MC", ["mc", "查服"])
+async def about(消息, data):
+    log.info("收到mc命令")
+    
+    服务器 = await mcstatus.JavaServer.async_lookup("ifeng.asia:1")
+    延迟 = await 服务器.async_ping()
+    状态 = await 服务器.async_status()
+    查询 = await 服务器.async_query()
+
+
+    await api.send_message(data, f"""服务器状态:
+延迟: {延迟}ms
+在线人数: {状态.players.online}/{状态.players.max}
+{状态}
+{查询}
+""")
