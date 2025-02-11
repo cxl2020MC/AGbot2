d82e117410bf43aa6a8703696f71d930d08342b7
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2025-02-11 09:46:49 +0000

    test

--- plugins/mc.py
+++ plugins/mc.py
@@ -12,12 +12,12 @@ async def about(消息, data):
     服务器 = await mcstatus.JavaServer.async_lookup("ifeng.asia:1")
     延迟 = await 服务器.async_ping()
     状态 = await 服务器.async_status()
-    查询 = await 服务器.async_query()
+    # 查询 = await 服务器.async_query()
 
 
     await api.send_message(data, f"""服务器状态:
 延迟: {延迟}ms
 在线人数: {状态.players.online}/{状态.players.max}
 {状态}
-{查询}
+
 """)
