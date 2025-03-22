d34838157c98bfc3c7aac5e7616ce1649e087ee1
cxl2020MC <andamu2019@163.com>
2025-03-22 17:37:57 +0800

    添加新群聊白名单ID 1035082529

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from plugins import command, help, about, mhy, ai, status, mc
 config.管理员QQ号 = [1176503930, 2130812665]
 config.机器人QQ号 = 1176503930
 config.群聊白名单 = [860769764, 463566277,
-                959593907, 925603600, 993426084, 456912162]
+                959593907, 925603600, 993426084, 456912162, 1035082529]
 
 
 plugin.load_pulgin(help.bot)
