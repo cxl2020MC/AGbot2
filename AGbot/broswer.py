a0a62b37bb9218a03beb404f00a5850dec83ed15
cxl2020MC <andamu2019@163.com>
2025-07-08 14:22:07 +0800

    refactor(broswer): 重构浏览器连接逻辑
    - 修改 main 函数，增加一个回调函数 func 参数，以支持更灵活的浏览器操作
    - 在浏览器连接成功后，添加成功日志记录
    - 改进代码结构，使浏览器连接和操作更加清晰和模块化

--- AGbot/broswer.py
+++ AGbot/broswer.py
@@ -2,8 +2,10 @@ from playwright.async_api import async_playwright, Playwright
 from . import config
 from .log import logger as log
 
-async def main():
+async def main(func):
     async with async_playwright() as p:
         log.info("连接浏览器")
         chromium = p.chromium
         broswer = await chromium.connect(config.playwright_chromium_endpoint)
+        log.info("连接浏览器成功")
+        return await func(broswer)
