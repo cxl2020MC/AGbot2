6ac043837797ab962b445e64cb8f8c292cb36c62
cxl2020MC <andamu2019@163.com>
2025-07-17 18:25:11 +0800

    feat(help): 优化帮助信息格式
    - 在插件列表中添加插件名称
    - 调整命令格式，使其更易读

--- plugins/help.py
+++ plugins/help.py
@@ -11,7 +11,9 @@ bot = plugin.Plugin("帮助")
 async def 帮助(event: MessageEvent):
     plugin_list = plugin.Plugin.plugin_list
     log.debug(f"plugin_list: {plugin_list}")
-    模板 = jinja2.Template("""帮助：{% for 插件 in 插件列表 %}{% for 命令 in 插件.command_list %}
+    模板 = jinja2.Template("""帮助：
+{% for 插件 in 插件列表 %}{{ 插件.name }}:
+{% for 命令 in 插件.command_list %}
     {{ 命令["命令名称"] }}: {{ ", ".join(命令["command_list"]) }}{% endfor %}{% endfor %}""")
     消息内容 = 模板.render(插件列表=plugin_list)
     await api.send_message(event, 消息内容)
