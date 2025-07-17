d6a6091d2ad332e3db3fa9812c2bd3e0d368ea58
cxl2020MC <andamu2019@163.com>
2025-07-17 18:10:47 +0800

    refactor(app): 注释掉 mhy 和 test 插件
    - 移除了 mhy 和 test 插件的导入语句
    - 注释掉了 mhy 和 test 插件的加载语句

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from AGbot import client, config, plugin
-from plugins import command, help, about, mhy, ai, status, mc, test
+from plugins import command, help, about, ai, status, mc
 
 config.管理员QQ号 = [1176503930, 2130812665]
 config.机器人QQ号 = 1176503930
@@ -11,11 +11,11 @@ config.playwright_chromium_endpoint = "ws://127.0.0.1:3456/cxl2020mc"
 plugin.load_pulgin(help.bot)
 plugin.load_pulgin(about.bot)
 plugin.load_pulgin(command.bot)
-plugin.load_pulgin(mhy.bot)
+# plugin.load_pulgin(mhy.bot)
 plugin.load_pulgin(ai.bot)
 plugin.load_pulgin(status.bot)
 plugin.load_pulgin(mc.bot)
-plugin.load_pulgin(test.bot)
+# plugin.load_pulgin(test.bot)
 
 
 client.run()
