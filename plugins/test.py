39378e780c1cede6b5a9cf188e5151dfb44219f7
cxl2020MC <andamu2019@163.com>
2025-07-21 22:10:15 +0800

    fix(plugins/test): 修复网页截图命令中获取参数的方式
    - 将 event.message 更改为 event.raw_message 以正确获取原始消息

--- plugins/test.py
+++ plugins/test.py
@@ -11,7 +11,7 @@ bot = plugin.Plugin("Test")
 
 @bot.command("网页截图", ["网页截图"])
 async def 网页截图(event: MessageEvent):
-    url = command_tools.Command(event.message).get_arg(1)
+    url = command_tools.Command(event.raw_message).get_arg(1)
     base64_image = await broswer.屏幕截图(url)
     log.debug(base64_image)
     await api.send_message(event, f"[CQ:image,file=base64://{base64_image}]")
