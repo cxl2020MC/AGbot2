e202e1cf82705fda4fecdfe8e3870d3c156ceab7
cxl2020MC <andamu2019@163.com>
2025-07-22 09:44:21 +0800

    添加类型注释

--- AGbot/event.py
+++ AGbot/event.py
@@ -1,16 +1,19 @@
-
+from . import api
 class Event:
     def __init__(self, data) -> None:
         self.data = data
 
 
 class MessageEvent(Event):
-    def __init__(self, data) -> None:
+    def __init__(self, data: dict) -> None:
         super().__init__(data)
-        self.message = data["message"]
-        self.raw_message = data["raw_message"]
+        self.message: str = data.get("message", "")
+        self.raw_message: list = data.get("raw_message", [])
         # self.message = self.raw_message
-        self.message_id = data["message_id"]
+        self.message_id: int | None = data.get("message_id")
+        self.sender: dict = data.get("sender", {})
+        self.user_id = self.sender.get("user_id")
+        self.sender_name = self.sender.get("nickname")
         
 
 
