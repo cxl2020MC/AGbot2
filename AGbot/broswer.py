81a272e59e85ba34a83141915ce1de05ee93c9c5
cxl2020MC <andamu2019@163.com>
2025-07-24 19:22:38 +0800

    refactor(broswer): 优化浏览器模块功能
    - 修改 remote_main 和 main 函数，增加日志信息
    - 在 main 函数中添加浏览器窗口大小设置
    - 屏幕截图函数中注释掉设置页面大小的代码

--- AGbot/broswer.py
+++ AGbot/broswer.py
@@ -3,34 +3,40 @@ import base64
 from . import config
 from .log import logger as log
 
+
 async def remote_main(func):
     async with async_playwright() as p:
         log.info("连接浏览器")
-        chromium = p.firefox # chromium
+        chromium = p.firefox  # chromium
         broswer = await chromium.connect(config.playwright_chromium_endpoint)
         log.info("连接浏览器成功")
         page = await broswer.new_page()
-        ret_data =  await func(page)
+        ret_data = await func(page)
         await page.close()
         return ret_data
+
 
 async def main(func):
     async with async_playwright() as p:
         log.info("启动浏览器")
         browser = await p.chromium.launch()
         log.info("启动浏览器成功")
+        log.info("设置浏览器窗口大小")
+        browser = await browser.new_context(viewport={"width": 1920, "height": 1080}, device_scale_factor=1.5)
+        log.info("设置浏览器窗口大小成功")
         page = await browser.new_page()
         ret_data = await func(page)
         log.info("关闭浏览器")
         await browser.close()
         return ret_data
 
+
 async def 屏幕截图(url, full_page=True):
     async def func(page):
         log.info(f"开始截图: {url}")
         await page.goto(url)
-        log.debug("设置页面大小")
-        await page.set_viewport_size({"width": 1920, "height": 1080})
+        # log.debug("设置页面大小")
+        # await page.set_viewport_size({"width": 1920, "height": 1080})
         log.debug("等待页面加载完成")
         await page.wait_for_load_state("networkidle")
         log.debug("页面加载完成")
