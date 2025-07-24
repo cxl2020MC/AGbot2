53826b45880cf1978e69aaec6803ffebb4e4a35f
cxl2020MC <andamu2019@163.com>
2025-07-24 19:45:05 +0800

    feat(test): 为网页截图功能添加超时设置
    - 在网页截图命令中增加了超时参数，默认为30000毫秒
    - 优化了代码结构，提高了可读性和可维护性

--- plugins/test.py
+++ plugins/test.py
@@ -1,3 +1,4 @@
+import time
 from AGbot import plugin
 from AGbot.event import MessageEvent
 from AGbot.log import logger as log
@@ -11,8 +12,10 @@ bot = plugin.Plugin("Test")
 
 @bot.command("网页截图", ["网页截图"])
 async def 网页截图(event: MessageEvent):
-    url = command_tools.Command(event.raw_message).get_arg(0)
+    command = command_tools.Command(event.raw_message)
+    url = command.get_arg(0)
+    timeout = int(command.get_arg(1, "30000") or 30000)
     log.debug(url)
-    base64_image = await broswer.屏幕截图(url)
+    base64_image = await broswer.屏幕截图(url, timeout=timeout)
     log.debug(base64_image)
     await api.send_message(event, f"[CQ:image,file=base64://{base64_image}]")
