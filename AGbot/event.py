5b66871d23c4c1fa14e09a1a8e08708b4182fdc1
cxl2020MC <andamu2019@163.com>
2025-07-28 16:15:09 +0800

    feat(event): 优化事件类结构并添加群消息事件类
    - 为 Event 类添加 type 和 sub_type 属性
    - 为 MessageEvent 类添加 message_type 属性
    - 新增 GroupMessageEvent 类，继承自 MessageEvent
    - 添加 group_id 和 anonymous 属性
    - 实现异步获取群名称的方法

--- AGbot/event.py
+++ AGbot/event.py
@@ -2,13 +2,16 @@ from . import api
 
 
 class Event:
-    def __init__(self, data) -> None:
-        self.data = data
+    def __init__(self, data: dict) -> None:
+        self.data: dict = data
+        self.type: str = data.get("post_type", "")
+        self.sub_type: str = data.get("sub_type", "")
 
 
 class MessageEvent(Event):
     def __init__(self, data: dict) -> None:
         super().__init__(data)
+        self.message_type: str = data.get("message_type", "")
         self.message: list = data.get("message", [])
         self.raw_message: str = data.get("raw_message", "")
         # self.message = self.raw_message
@@ -16,7 +19,34 @@ class MessageEvent(Event):
         self.sender: dict = data.get("sender", {})
         self.user_id: int | None = self.sender.get("user_id")
         self.sender_nickname: str | None = self.sender.get("nickname")
-        self.sender_card: str | None = self.sender.get("card")
 
     def get_username(self) -> str:
         return self.sender.get("card", "") or self.sender.get("nickname", "")
+
+
+class GroupMessageEvent(MessageEvent):
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
+        self.group_id: int | None = data.get("group_id")
+        # self.group_name: str | None = api.获取群名称(self.group_id)
+        self.anonymous: dict | None = data.get("anonymous")
+        self.sender_card: str | None = self.sender.get("card")
+
+    @property
+    async def group_name(self) -> str:
+        return await api.获取群名称(self.group_id)
+
+
+class Sender:
+    def __init__(self, sender: dict) -> None:
+        self.user_id: int | None = sender.get("user_id")
+        self.nickname: str | None = sender.get("nickname")
+
+
+class GroupSender(Sender):
+    def __init__(self, sender: dict) -> None:
+        super().__init__(sender)
+
+        self.card: str | None = sender.get("card")
+        self.sex: str | None = sender.get("sex")
+        self.age: int | None = sender.get("age")
