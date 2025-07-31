97c86734f89ff243bf256c60d167e83e7f21ec2d
cxl2020MC <andamu2019@163.com>
2025-07-31 14:38:24 +0800

    refactor(AGbot): 更新 AGbot 以使用远程 Playwright 实例
    - 将 broswer.py 中的 main 函数调用改为 remote_main
    - 在 app.py 中更新 Playwright Chromium 端点地址为 localhost

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ config.管理员QQ号 = [1176503930, 2130812665]
 config.机器人QQ号 = 1176503930
 config.群聊白名单 = [860769764, 463566277,
                 959593907, 925603600, 993426084, 456912162, 1035082529, 1045889338]
-# config.playwright_chromium_endpoint = "ws://127.0.0.1:3456/cxl2020mc"
+config.playwright_chromium_endpoint = "ws://localhost:3456/cxl2020mc"
 
 
 plugin.load_pulgin(help.bot)
