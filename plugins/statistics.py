a46f09f84925357ceff1b49b1e840ffb8af3372f
cxl2020MC <andamu2019@163.com>
2025-08-16 10:43:26 +0800

    feat(statistics): 添加统计命令并优化事件处理函数命名
    - 新增统计命令，回复 bot 运行时间和群消息数
    - 重命名消息事件处理函数，提高代码可读性

--- plugins/statistics.py
+++ plugins/statistics.py
@@ -11,12 +11,16 @@ bot = plugin.Plugin("统计")
 start_time = time.time()
 data = {
     "group_message_count": 0,
-
 }
 
 @bot.on_message("group")
-async def 统计(event: GroupMessageEvent) -> None:
+async def 事件统计(event: GroupMessageEvent) -> None:
     # log.info(f"{event.get_username()}在群{await api.获取群名称(event.group_id)}发送了消息")
     data["group_message_count"] += 1
     log.debug(f"群消息数: {data['group_message_count']}")
 
+@bot.command("统计", ["统计"])
+async def 统计(event: GroupMessageEvent) -> None:
+    run_time = time.time() - start_time
+    await api.send_message(event, f"""bot运行时间: {int(run_time/60/60/24)}天{int(run_time/60/60%24)}小时{int(run_time/60%60)}分{int(run_time%60)}秒
+群消息数: {data['group_message_count']}""")
