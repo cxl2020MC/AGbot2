7a645bb6810afd44083acb90eb22d3d356cfa77c
cxl2020MC <andamu2019@163.com>
2025-09-19 18:27:11 +0800

    fix(ai): 修复ai聊天记录
    - 将 add_chat_history 返回值转换为列表

--- plugins/ai.py
+++ plugins/ai.py
@@ -51,7 +51,7 @@ async def ai(event: GroupMessageEvent):
     message = {'role': 'user',
                'content': f"{event.get_username()} ({event.user_id}): {raw_message} [{event.message_id}]"}
 
-    messages = add_chat_history(event.group_id, message).copy()
+    messages = list(add_chat_history(event.group_id, message))
     log.debug(f"聊天记录: {messages}")
     messages.insert(0, {'role': 'system', 'content': system_format.format(group_name=await event.group_name, self_id=event.self_id)})
 
