3815b52dfd798c45753576584d0969d3019d5b24
cxl2020MC <andamu2019@163.com>
2026-02-25 18:34:37 +0800

    feat(mhy_core): 重构启动器API配置和类型定义
    - 添加LaucherArea和Laucher枚举类型用于规范启动器区域和服务商
    - 定义类型字面量LauncherLiteral和LaucherAreaLiteral提高类型安全
    - 重构get_hyp_api_url函数使用字典映射替代match语句
    - 重构get_launcher_id函数使用字典映射替代match语句
    - 添加Launcher类提供启动器相关方法封装
    - 创建HYP_API_DATA常量存储启动器配置数据
    - 添加create_launcher工厂函数创建启动器实例
    - 注释掉临时无用的代码片段

--- plugins/mhy_core.py
+++ plugins/mhy_core.py
@@ -2,39 +2,87 @@
 import asyncio
 import httpx
 import json
+from typing import Literal, TypedDict
+from enum import Enum
+
 from AGbot.log import logger as log
 # from  import models
 
 
+class LaucherArea(Enum):
+    "启动器api地区"
+    CN = 1
+    GLOBAL = 2
+
+
+type LaucherAreaLiteral = Literal[LaucherArea.CN, LaucherArea.GLOBAL]
+
 client = httpx.AsyncClient()
 
 
-def get_hyp_api_url(area: str, api: str) -> str:
-    match area:
-        case "国服":
-            host = "hyp-api.mihoyo.com"
-        case "国际服":
-            host = "sg-hyp-api.hoyoverse.com"
-        case _:
-            raise Exception("不支持的启动器")
+# def get_hyp_api_url(area: str, api: str) -> str:
+#     match area:
+#         case "国服":
+#             host = "hyp-api.mihoyo.com"
+#         case "国际服":
+#             host = "sg-hyp-api.hoyoverse.com"
+#         case _:
+#             raise Exception("不支持的启动器")
+#     url = f"https://{host}/hyp/hyp-connect/api/{api}"
+#     return url
+
+
+def get_hyp_api_url(area, api: str) -> str:
+    hyp_api_bases = {
+        LaucherArea.CN: "hyp-api.mihoyo.com",
+        LaucherArea.GLOBAL: "sg-hyp-api.hoyoverse.com",
+    }
+    host = hyp_api_bases.get(area)
+    if host is None:
+        raise Exception("不支持的启动器")
     url = f"https://{host}/hyp/hyp-connect/api/{api}"
     return url
 
+# def get_launcher_id(laucher: str) -> str:
+#     match laucher:
+#         case "国服":
+#             return "jGHBHlcOq1"
+#         case "国际服":
+#             return "VYTpXlbWo8"
+#         case "B服原神":
+#             return "umfgRO5gh5"
+#         case "B服星铁":
+#             return "6P5gHMNyK3"
+#         case "B服绝区零":
+#             return "xV0f4r1GT0"
+#         case _:
+#             raise Exception("不支持的启动器")
 
-def get_launcher_id(laucher: str) -> str:
-    match laucher:
-        case "国服":
-            return "jGHBHlcOq1"
-        case "国际服":
-            return "VYTpXlbWo8"
-        case "B服原神":
-            return "umfgRO5gh5"
-        case "B服星铁":
-            return "6P5gHMNyK3"
-        case "B服绝区零":
-            return "xV0f4r1GT0"
-        case _:
-            raise Exception("不支持的启动器")
+
+class Laucher(Enum):
+    CN = 1
+    GLOBAL = 2
+    BILIBILIYS = 3
+    BILIBILISR = 4
+    BILIBILIZZZ = 5
+
+
+type LauncherLiteral = Literal[Laucher.CN, Laucher.GLOBAL,
+                               Laucher.BILIBILIYS, Laucher.BILIBILISR, Laucher.BILIBILIZZZ]
+
+
+def get_launcher_id(laucher: LauncherLiteral) -> str:
+    launcher_ids = {
+        Laucher.CN: "jGHBHlcOq1",
+        Laucher.GLOBAL: "VYTpXlbWo8",
+        Laucher.BILIBILIYS: "umfgRO5gh5",
+        Laucher.BILIBILISR: "6P5gHMNyK3",
+        Laucher.BILIBILIZZZ: "xV0f4r1GT0",
+    }
+    laucher_id = launcher_ids.get(laucher)
+    if not laucher_id:
+        raise Exception("不支持的启动器")
+    return laucher_id
 
 
 def get_laucher_data(laucher: str) -> dict:
@@ -42,16 +90,6 @@ def get_laucher_data(laucher: str) -> dict:
 
 
 async def get_game_id(laucher: str, game: str) -> str:
-    # if game == "原神":
-    #     return "1Z8W5NHUQb"
-    # elif game == "绝区零":
-    #     return "x6znKlJ0xK"
-    # elif game == "崩坏星穹铁道":
-    #     return "64kMb5iAWu"
-    # elif game == "崩坏3":
-    #     return "osvnlOc0S8"
-    # else:
-    #     raise Exception("不支持的游戏")
     data = await 获取全部游戏(laucher)
     for i in data["data"]["games"]:
         if i["biz"] == game:
@@ -65,6 +103,72 @@ def get_hyp_api_params(laucher: str = "国服", language: str = "zh-cn") -> dict
         "language": language,
         # "game_id": get_game_id(游戏)
     }
+
+
+class Launcher:
+    def __init__(self, area, launcher, api_base: str, launcher_id: str, language: str = "zh-cn"):
+        self.area = area
+        self.launcher = launcher
+        self.api_base = api_base
+        self.launcher_id = launcher_id
+        self.language = language
+
+    @staticmethod
+    def _get_launcher_id(laucher: LauncherLiteral) -> str:
+        match laucher:
+            case Laucher.CN:
+                return "jGHBHlcOq1"
+            case Laucher.GLOBAL:
+                return "VYTpXlbWo8"
+            case Laucher.BILIBILIYS:
+                return "umfgRO5gh5"
+            case Laucher.BILIBILISR:
+                return "6P5gHMNyK3"
+            case Laucher.BILIBILIZZZ:
+                return "xV0f4r1GT0"
+            case _:
+                raise Exception("不支持的启动器")
+
+    @staticmethod
+    def get_hyp_api_base(area: LaucherAreaLiteral) -> str:
+        # hyp_api_bases = {
+        #     LaucherArea.CN: "hyp-api.mihoyo.com",
+        #     LaucherArea.GLOBAL: "sg-hyp-api.hoyoverse.com",
+        # }
+        # host = hyp_api_bases.get(area)
+        # if host is None:
+        #     raise Exception("不支持的启动器")
+        # return host
+        match area:
+            case LaucherArea.CN:
+                return "hyp-api.mihoyo.com"
+            case LaucherArea.GLOBAL:
+                return "sg-hyp-api.hoyoverse.com"
+            case _:
+                raise Exception("不支持的启动器")
+
+
+HYP_API_DATA = {
+    LaucherArea.CN: {
+        "api_base": "hyp-api.mihoyo.com",
+        "launcher_ids": {
+            Laucher.CN: "jGHBHlcOq1",
+            Laucher.BILIBILIYS: "umfgRO5gh5",
+            Laucher.BILIBILISR: "6P5gHMNyK3",
+            Laucher.BILIBILIZZZ: "xV0f4r1GT0",
+        }
+    },
+    LaucherArea.GLOBAL: {
+        "api_base": "hyp-api.mihoyo.com",
+        "launcher_ids": {
+            Laucher.GLOBAL: "VYTpXlbWo8",
+        }
+    },
+}
+
+
+def create_launcher(laucher: Laucher, area: LaucherArea):
+    return Launcher(HYP_API_DATA[area]["api_base"], HYP_API_DATA[area]["launcher_ids"][laucher])
 
 
 # https://hyp-api.mihoyo.com/hyp/hyp-connect/api/getGameContent?launcher_id=jGHBHlcOq1&game_id=1Z8W5NHUQb&language=zh-cn
