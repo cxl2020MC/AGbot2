a035a1137c19b25c96474363401d82212fb9d56e
cxl2020MC <andamu2019@163.com>
2026-06-25 19:53:32 +0800

    更新日志系统与依赖

--- AGbot/log.py
+++ AGbot/log.py
@@ -1,12 +1,17 @@
-from loguru import logger
 import sys
+
+from loguru import logger
 
 logger.remove()
 
-log_format = "{time:YYYY-MM-DD HH:mm:ss} [ <lvl>{level:^8}</lvl> ] {name}:{function}:{line} | <lvl>{message}</lvl>"
+log_format = "{time:YYYY-MM-DD HH:mm:ss} | <lvl>{level:^8}</lvl> | {message} | {name}:{function}:{line}"
 
 logger.add(sys.stdout, colorize=True, format=log_format, level="DEBUG")
-logger.add("logs/app.log", rotation="10MB", retention="30 days", level="DEBUG", format=log_format, enqueue=True)
+logger.add("logs/debug.log", rotation="10MB", retention="30 days", level="DEBUG", format=log_format, enqueue=True)
+logger.add("logs/info.log", rotation="10MB", retention="30 days", level="INFO", format=log_format, enqueue=True)
+logger.add("logs/error.log", rotation="10MB", retention="30 days", level="ERROR", format=log_format, enqueue=True)
+
+
 
 # logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
 
